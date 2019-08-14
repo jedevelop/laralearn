5a9ef2c025b88cef159362e5ab997dc1e2bf2ff2
mikhailov.m <mikhailov.miail@yandex.ru>
2019-08-14 22:39:31 +0300

    lesson10

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,11 +12,11 @@
                         <div class="relative">
                             <label for="email" class="uppercase text-blue-500 text-xs font-bold absolute pl-3 pt-2">E-mail</label>
 
-                            <div class="">
+                            <div>
                                 <input id="email" type="email" class="pt-8 w-full rounded p-3  bg-blue-800 text-gray-100 outline-none focus:bg-blue-700" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="your@email.com">
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                                    <span class="text-red-600 text-sm pt-1" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
@@ -31,7 +31,7 @@
                                 placeholder="Password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                                    <span class="text-red-600 text-sm pt-1" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
@@ -39,38 +39,25 @@
                         </div>
 
                         <div class="pt-2">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                   <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="text-white" for="remember">
                                         {{ __('Remember Me') }}
                                     </label>
-                                </div>
-                            </div>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                        <div class="pt-8"> 
+                            <button type="submit" class="w-full bg-gray-300 py-2 px-3 text-left uppercase rounded text-blue-800 font-bold">
+                                   Login
+                            </button>
+                        </div>
+                        <div class="flex justify-between pt-8 text-white text-sm font-bold">
+                                    <a class="" href="{{ route('password.request') }}">
+                                        Forgot Your Password
+                                      <a class="" href="{{ route('password.request') }}">
+                                        Register
                                     </a>
-                                @endif
-                            </div>
                         </div>
                     </form>
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-body">
-                    
-                </div>
-            </div>
-        </div>
     </div>
 </div>
 @endsection
